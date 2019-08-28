e929f0c1db41ece7fc9f02065c3578bd2531c174
Krzysztof Łagan <krzysztof.lagan@nm.com.pl>
2019-08-28 08:17:58 +0200

    FIX#A4 nie pokazywanie nazw userów nie mających przesłanych odpowiedzi

--- src/app/report/ReportBodyRanking.js
+++ src/app/report/ReportBodyRanking.js
@@ -6,7 +6,7 @@ export default class ReportBodyRanking extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            showEmptyUsers: true
+            showEmptyUsers: false
         };
     }
  
